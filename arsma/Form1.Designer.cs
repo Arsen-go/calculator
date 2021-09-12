5c960d6f104e4e298484dd8367caf5b0da7a2988
Arsen-go <68658606+Arsen-go@users.noreply.github.com>
2021-09-12 16:28:05 +0400

    Add project files.

---
+++ arsma/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿
+namespace arsma
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.button0 = new System.Windows.Forms.Button();
+			this.area = new System.Windows.Forms.TextBox();
+			this.sum = new System.Windows.Forms.Label();
+			this.number1 = new System.Windows.Forms.Button();
+			this.plus = new System.Windows.Forms.Button();
+			this.equal = new System.Windows.Forms.Button();
+			this.number3 = new System.Windows.Forms.Button();
+			this.number2 = new System.Windows.Forms.Button();
+			this.dot = new System.Windows.Forms.Button();
+			this.twoZero = new System.Windows.Forms.Button();
+			this.number4 = new System.Windows.Forms.Button();
+			this.number6 = new System.Windows.Forms.Button();
+			this.number5 = new System.Windows.Forms.Button();
+			this.minus = new System.Windows.Forms.Button();
+			this.number9 = new System.Windows.Forms.Button();
+			this.square = new System.Windows.Forms.Button();
+			this.number7 = new System.Windows.Forms.Button();
+			this.number8 = new System.Windows.Forms.Button();
+			this.divide = new System.Windows.Forms.Button();
+			this.multiply = new System.Windows.Forms.Button();
+			this.armat = new System.Windows.Forms.Button();
+			this.dateAndTime = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.clean = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button0
+			// 
+			this.button0.Location = new System.Drawing.Point(12, 359);
+			this.button0.Name = "button0";
+			this.button0.Size = new System.Drawing.Size(50, 40);
+			this.button0.TabIndex = 0;
+			this.button0.Text = "0";
+			this.button0.UseVisualStyleBackColor = true;
+			this.button0.Click += new System.EventHandler(this.number_Click);
+			// 
+			// area
+			// 
+			this.area.Location = new System.Drawing.Point(12, 37);
+			this.area.Multiline = true;
+			this.area.Name = "area";
+			this.area.Size = new System.Drawing.Size(218, 30);
+			this.area.TabIndex = 1;
+			// 
+			// sum
+			// 
+			this.sum.BackColor = System.Drawing.Color.Transparent;
+			this.sum.Location = new System.Drawing.Point(12, 13);
+			this.sum.Name = "sum";
+			this.sum.Size = new System.Drawing.Size(141, 21);
+			this.sum.TabIndex = 2;
+			// 
+			// number1
+			// 
+			this.number1.Location = new System.Drawing.Point(12, 313);
+			this.number1.Name = "number1";
+			this.number1.Size = new System.Drawing.Size(50, 40);
+			this.number1.TabIndex = 3;
+			this.number1.Text = "1";
+			this.number1.UseVisualStyleBackColor = true;
+			this.number1.Click += new System.EventHandler(this.number_Click);
+			// 
+			// plus
+			// 
+			this.plus.Location = new System.Drawing.Point(180, 266);
+			this.plus.Name = "plus";
+			this.plus.Size = new System.Drawing.Size(50, 41);
+			this.plus.TabIndex = 4;
+			this.plus.Text = "+";
+			this.plus.UseVisualStyleBackColor = true;
+			this.plus.Click += new System.EventHandler(this.operation_Click);
+			// 
+			// equal
+			// 
+			this.equal.Location = new System.Drawing.Point(180, 312);
+			this.equal.Name = "equal";
+			this.equal.Size = new System.Drawing.Size(50, 86);
+			this.equal.TabIndex = 5;
+			this.equal.Text = "=";
+			this.equal.UseVisualStyleBackColor = true;
+			this.equal.Click += new System.EventHandler(this.equal_Click);
+			// 
+			// number3
+			// 
+			this.number3.Location = new System.Drawing.Point(124, 312);
+			this.number3.Name = "number3";
+			this.number3.Size = new System.Drawing.Size(50, 40);
+			this.number3.TabIndex = 6;
+			this.number3.Text = "3";
+			this.number3.UseVisualStyleBackColor = true;
+			this.number3.Click += new System.EventHandler(this.number_Click);
+			// 
+			// number2
+			// 
+			this.number2.Location = new System.Drawing.Point(68, 313);
+			this.number2.Name = "number2";
+			this.number2.Size = new System.Drawing.Size(50, 40);
+			this.number2.TabIndex = 7;
+			this.number2.Text = "2";
+			this.number2.UseVisualStyleBackColor = true;
+			this.number2.Click += new System.EventHandler(this.number_Click);
+			// 
+			// dot
+			// 
+			this.dot.Location = new System.Drawing.Point(124, 358);
+			this.dot.Name = "dot";
+			this.dot.Size = new System.Drawing.Size(50, 40);
+			this.dot.TabIndex = 8;
+			this.dot.Text = ",";
+			this.dot.UseVisualStyleBackColor = true;
+			this.dot.Click += new System.EventHandler(this.dot_Click);
+			// 
+			// twoZero
+			// 
+			this.twoZero.Location = new System.Drawing.Point(68, 359);
+			this.twoZero.Name = "twoZero";
+			this.twoZero.Size = new System.Drawing.Size(50, 40);
+			this.twoZero.TabIndex = 9;
+			this.twoZero.Text = "00";
+			this.twoZero.UseVisualStyleBackColor = true;
+			this.twoZero.Click += new System.EventHandler(this.number_Click);
+			// 
+			// number4
+			// 
+			this.number4.Location = new System.Drawing.Point(12, 267);
+			this.number4.Name = "number4";
+			this.number4.Size = new System.Drawing.Size(50, 40);
+			this.number4.TabIndex = 10;
+			this.number4.Text = "4";
+			this.number4.UseVisualStyleBackColor = true;
+			this.number4.Click += new System.EventHandler(this.number_Click);
+			// 
+			// number6
+			// 
+			this.number6.Location = new System.Drawing.Point(124, 266);
+			this.number6.Name = "number6";
+			this.number6.Size = new System.Drawing.Size(50, 40);
+			this.number6.TabIndex = 11;
+			this.number6.Text = "6";
+			this.number6.UseVisualStyleBackColor = true;
+			this.number6.Click += new System.EventHandler(this.number_Click);
+			// 
+			// number5
+			// 
+			this.number5.Location = new System.Drawing.Point(68, 267);
+			this.number5.Name = "number5";
+			this.number5.Size = new System.Drawing.Size(50, 40);
+			this.number5.TabIndex = 12;
+			this.number5.Text = "5";
+			this.number5.UseVisualStyleBackColor = true;
+			this.number5.Click += new System.EventHandler(this.number_Click);
+			// 
+			// minus
+			// 
+			this.minus.Location = new System.Drawing.Point(180, 221);
+			this.minus.Name = "minus";
+			this.minus.Size = new System.Drawing.Size(50, 40);
+			this.minus.TabIndex = 13;
+			this.minus.Text = "-";
+			this.minus.UseVisualStyleBackColor = true;
+			this.minus.Click += new System.EventHandler(this.operation_Click);
+			// 
+			// number9
+			// 
+			this.number9.Location = new System.Drawing.Point(124, 221);
+			this.number9.Name = "number9";
+			this.number9.Size = new System.Drawing.Size(50, 40);
+			this.number9.TabIndex = 14;
+			this.number9.Text = "9";
+			this.number9.UseVisualStyleBackColor = true;
+			this.number9.Click += new System.EventHandler(this.number_Click);
+			// 
+			// square
+			// 
+			this.square.Location = new System.Drawing.Point(68, 175);
+			this.square.Name = "square";
+			this.square.Size = new System.Drawing.Size(50, 40);
+			this.square.TabIndex = 15;
+			this.square.Text = "^2";
+			this.square.UseVisualStyleBackColor = true;
+			this.square.Click += new System.EventHandler(this.operation_Click);
+			// 
+			// number7
+			// 
+			this.number7.Location = new System.Drawing.Point(12, 221);
+			this.number7.Name = "number7";
+			this.number7.Size = new System.Drawing.Size(50, 40);
+			this.number7.TabIndex = 16;
+			this.number7.Text = "7";
+			this.number7.UseVisualStyleBackColor = true;
+			this.number7.Click += new System.EventHandler(this.number_Click);
+			// 
+			// number8
+			// 
+			this.number8.Location = new System.Drawing.Point(68, 221);
+			this.number8.Name = "number8";
+			this.number8.Size = new System.Drawing.Size(50, 40);
+			this.number8.TabIndex = 17;
+			this.number8.Text = "8";
+			this.number8.UseVisualStyleBackColor = true;
+			this.number8.Click += new System.EventHandler(this.number_Click);
+			// 
+			// divide
+			// 
+			this.divide.Location = new System.Drawing.Point(124, 175);
+			this.divide.Name = "divide";
+			this.divide.Size = new System.Drawing.Size(50, 40);
+			this.divide.TabIndex = 18;
+			this.divide.Text = "/";
+			this.divide.UseVisualStyleBackColor = true;
+			this.divide.Click += new System.EventHandler(this.operation_Click);
+			// 
+			// multiply
+			// 
+			this.multiply.Location = new System.Drawing.Point(180, 175);
+			this.multiply.Name = "multiply";
+			this.multiply.Size = new System.Drawing.Size(50, 40);
+			this.multiply.TabIndex = 19;
+			this.multiply.Text = "*";
+			this.multiply.UseVisualStyleBackColor = true;
+			this.multiply.Click += new System.EventHandler(this.operation_Click);
+			// 
+			// armat
+			// 
+			this.armat.Location = new System.Drawing.Point(12, 175);
+			this.armat.Name = "armat";
+			this.armat.Size = new System.Drawing.Size(50, 40);
+			this.armat.TabIndex = 20;
+			this.armat.Text = "^½";
+			this.armat.UseVisualStyleBackColor = true;
+			this.armat.Click += new System.EventHandler(this.operation_Click);
+			// 
+			// dateAndTime
+			// 
+			this.dateAndTime.Location = new System.Drawing.Point(124, 70);
+			this.dateAndTime.Name = "dateAndTime";
+			this.dateAndTime.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.dateAndTime.Size = new System.Drawing.Size(106, 23);
+			this.dateAndTime.TabIndex = 21;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// clean
+			// 
+			this.clean.Location = new System.Drawing.Point(180, 129);
+			this.clean.Name = "clean";
+			this.clean.Size = new System.Drawing.Size(50, 40);
+			this.clean.TabIndex = 22;
+			this.clean.Text = "C";
+			this.clean.UseVisualStyleBackColor = true;
+			this.clean.Click += new System.EventHandler(this.cleanButton);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(246, 411);
+			this.Controls.Add(this.clean);
+			this.Controls.Add(this.dateAndTime);
+			this.Controls.Add(this.armat);
+			this.Controls.Add(this.multiply);
+			this.Controls.Add(this.divide);
+			this.Controls.Add(this.number8);
+			this.Controls.Add(this.number7);
+			this.Controls.Add(this.square);
+			this.Controls.Add(this.number9);
+			this.Controls.Add(this.minus);
+			this.Controls.Add(this.number5);
+			this.Controls.Add(this.number6);
+			this.Controls.Add(this.number4);
+			this.Controls.Add(this.twoZero);
+			this.Controls.Add(this.dot);
+			this.Controls.Add(this.number2);
+			this.Controls.Add(this.number3);
+			this.Controls.Add(this.equal);
+			this.Controls.Add(this.plus);
+			this.Controls.Add(this.number1);
+			this.Controls.Add(this.sum);
+			this.Controls.Add(this.area);
+			this.Controls.Add(this.button0);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button0;
+		private System.Windows.Forms.TextBox area;
+		private System.Windows.Forms.Label sum;
+		private System.Windows.Forms.Button number1;
+		private System.Windows.Forms.Button plus;
+		private System.Windows.Forms.Button equal;
+		private System.Windows.Forms.Button number3;
+		private System.Windows.Forms.Button number2;
+		private System.Windows.Forms.Button dot;
+		private System.Windows.Forms.Button twoZero;
+		private System.Windows.Forms.Button number4;
+		private System.Windows.Forms.Button number6;
+		private System.Windows.Forms.Button number5;
+		private System.Windows.Forms.Button minus;
+		private System.Windows.Forms.Button number9;
+		private System.Windows.Forms.Button square;
+		private System.Windows.Forms.Button number7;
+		private System.Windows.Forms.Button number8;
+		private System.Windows.Forms.Button divide;
+		private System.Windows.Forms.Button multiply;
+		private System.Windows.Forms.Button armat;
+		public System.Windows.Forms.Label dateAndTime;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button clean;
+	}
+}
+
---
+++ arsma/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arsma
+{
+	public partial class Form1 : Form
+	{
+		Double result = 0;
+		String operation = null;
+		bool isOperationSelected = false;
+		bool isSelectedEqual = false;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void number_Click(object sender, EventArgs e)
+		{
+			Button number1 = (Button)sender;
+			area.Text += number1.Text;
+			isSelectedEqual = false;
+			if (isOperationSelected)
+			{
+				sum.Text += number1.Text;
+			} else
+			{
+				sum.Text = "";
+			}
+		}
+
+		private void operation_Click(object sender, EventArgs e)
+		{
+			Button operationSelected = (Button)sender;
+			if(area.Text == "")
+			{
+				return;
+			}
+			if(!isOperationSelected)
+			{
+				operation = operationSelected.Text;
+				result = Double.Parse(area.Text);
+				area.Clear();
+				sum.Text = result + " " + operation + " ";
+				isOperationSelected = true;
+				return;
+			}
+		}
+
+		private void equal_Click(object sender, EventArgs e)
+		{
+			if ((area.Text == "" && !(operation == "^2" || operation == "^½") || isSelectedEqual))
+			{
+				return;
+			}
+			switch (operation)
+			{
+				case "+":
+					result += Double.Parse(area.Text);
+					operationHelper();
+					break;
+				case "-":
+					result -= Double.Parse(area.Text);
+					operationHelper();
+					break;
+				case "*":
+					result *= Double.Parse(area.Text);
+					operationHelper();
+					break;
+				case "/":
+					result /= Double.Parse(area.Text);
+					operationHelper();
+					break;
+				case "^2":
+					result *= result;
+					operationHelper();
+					break;
+				case "^½":
+					result = Math.Sqrt(result);
+					operationHelper();
+					break;
+			}
+		}
+
+		private void operationHelper()
+		{
+			sum.Text += " = " + result.ToString();
+			isOperationSelected = false;
+			area.Text = result.ToString();
+			isSelectedEqual = true;
+		}
+
+		private void dot_Click(object sender, EventArgs e)
+		{
+			Button dot = (Button)sender;
+			if (area.Text.Contains(",") || area.Text.Length == 0)
+			{
+				return;
+			}
+			area.Text += dot.Text;
+
+			if (isOperationSelected)
+			{
+				sum.Text += dot.Text;
+			}
+			else
+			{
+				sum.Text = "";
+			}
+		}
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			dateAndTime.Text = DateTime.Now.ToString();
+		}
+
+		private void cleanButton(object sender, EventArgs e)
+		{
+			result = 0;
+			sum.Text = "";
+			isOperationSelected = false;
+			area.Text = "";
+		}
+	}
+}
